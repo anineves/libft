ecad1213ec947a48f72c17c739030cdc0d85e405
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-01 12:37:14 +0000

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,5 +12,27 @@
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
+int	i;
 
+	if (!dst || !src)
+		return (NULL);
+	if (dst > src)
+	{
+		i = (int)n - 1;
+		while (i >= 0)
+		{
+			*(char*)(dst + i) = *(char*)(src + i);
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < (int)n)
+		{
+			*(char*)(dst + i) = *(char*)(src + i);
+			i++;
+		}
+	}
+	return (dst);
 }
