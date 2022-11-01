ba5193583e375eb707f41aa5940c18e00ae80e38
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-01 12:35:32 +0000

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,6 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_calloc(size_t nmemb, size_t size){
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+void	*ptr;
+
+	ptr = (void*)malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmemb);
+	return (ptr);  
 
 }
