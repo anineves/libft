28e92a917f48236a146f0fbacae8ac680fa3c224
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-07 13:00:15 +0000

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,7 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
+#include <string.h>
+#include <stdio.h>
 
+void    *ft_memcpy(void *dest, const void *src, size_t n)
+{
+        size_t  i;
+        char    *pt_dest;
+        char    *pt_src;
+
+        pt_dest = (char *)dest;
+        pt_src = (char *)src;
+        i = 0;
+
+        while (i < n)
+        {
+                pt_dest[i] = pt_src[i];
+                i++;
+        }
+        return (dest);
 }
+/*
+int main ()
+{
+        int arr1[] = {3, 4};
+        int arr2[] = {1, 2};
+        printf("%d %d", arr2[0], arr2[1]);
+        ft_memcpy (arr2, arr1, 2 * sizeof(int));
+        printf("%d %d\n", arr2[0], arr2[1]);
+        return (0);
+}
+*/
