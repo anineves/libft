5ae3aab66ddcaa3e13a8c0200b5e753bd26e7e57
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-07 10:34:28 +0000

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -10,7 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strrchr(const char *s, int c)
-{
 
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
+
+char    *ft_strrchr(const char *s, int c)
+{
+        unsigned int    i;
+  
+        i = ft_strlen(s);
+  
+        if (c == '\0')
+                return ((char *)&s[i]);
+  
+        while (i > 0)
+        {
+                if (s[i] == (char)c)
+                        return ((char *)&s[i]);
+                i--;
+        }
+        return (NULL);
+}
+
+
+int main()
+{
+    const char *s = "Olaa";
+    char  c;
+
+    c = 'a';
+    printf("letra :%c\n", *ft_strchr(s, c));
+    //*ft_strchr(str, c);
+    return (0);
+}
+      
