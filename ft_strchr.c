2598775e5df2bf1e992d20a52ba24ad276ed962e
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-07 10:13:27 +0000

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -13,27 +13,31 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char *ft_strchr(const char *s, int c)
+
+char    *ft_strchr(const char *s, int c)
 {
-	while (*s != '\0')
-	{
-		if ((unsigned char)*s == (unsigned char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == 0)
-		return ((char *)s);
-	return (NULL);
+        unsigned int    i;
+
+        i = 0;
+        while (s[i])
+        {
+                if (s[i] == (char)c)
+                        return ((char *)&s[i]);
+                i++;
+        }
+        if ((char)c == s[i])
+                return ((char *)&s[i]);
+        return (NULL);
 }
 
-//retorna a primeira ocorrencia 
-int main() 
+
+int main()
 {
-    const char *str = "Olaa";
+    const char *s = "Olaa";
     char  c;
- 
-    c = 'l';
-    printf("letra :%c\n", *ft_strchr(str, c));
+
+    c = 'a';
+    printf("letra :%c\n", *ft_strchr(s, c));
     //*ft_strchr(str, c);
     return (0);
 }
