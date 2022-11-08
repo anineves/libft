2a45395d1d60ea24a6a900f7cc9756efb6123157
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-08 22:19:16 +0000

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -19,11 +19,11 @@ char	*ft_strrchr(const char *s, int c)
 
 	i = ft_strlen(s);
 	if (c == '\0')
-		return ((char *)&s[i]);
-	while (i > 0)
+		return ((char *)s + i);
+	while (i >= 0)
 	{
 		if (s[i] == (char)c)
-			return ((char *)&s[i]);
+			return ((char *)s + i);
 		i--;
 	}
 	return (NULL);
