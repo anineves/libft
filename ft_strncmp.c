843ee9734fedce133936a49e2e59b7240ec3adf3
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-08 22:34:22 +0000

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,20 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-        while (n > 0 && *s1 == *s2 && *s1)
-        {
-                s1++;
-                s2++;
-                n--;
-        }
-        return (*s1 - *s2);
+	size_t i;
 
+	i = 0;
+        
+	while (i < n && (s1[i] != '\0' || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return (((unsigned char*)s1)[i] - ((unsigned char*)s2)[i]);
+		i++;
+	}
+	return (0);
 }
+
 
 int             main(void)
 {
