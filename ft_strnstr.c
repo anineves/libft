154e29c1b2746c6f046c6c95690eed3a83fd78c4
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-08 22:42:46 +0000

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,8 +17,9 @@ char    *ft_strnstr(const char *s1, const char *s2, size_t len)
 {
         size_t          i;
         size_t          j;
-
         i = 0;
+        if((!s1 || !s2) && len == 0)
+                return (NULL);
         if (*s2 == '\0' )
                 return ((char*)s1);
         while (s1[i] != '\0' && i < len)
