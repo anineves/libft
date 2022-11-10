c4a086fec90eaa28de531837f859d41f3048cf84
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-10 17:45:14 +0000

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -19,7 +19,11 @@ char    *ft_strtrim(char const *s1, char const *set)
         size_t  i;
         size_t  start;
         size_t  end;
-
+       
+        if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
         i = 0;
         start = 0;
         end = ft_strlen(s1);
