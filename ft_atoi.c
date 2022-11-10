2733811f8daa1135c9b41d9cab3fcb055b621a3f
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-10 19:06:47 +0000

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,27 +11,29 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
-int	ft_atoi(const char *nptr)
-{
-	int	neg;
-	int	n;
 
-	n = 0;
-	neg = 0;
-	while (*str == '\t' || *str == '\n' || *str == '\v' || \
-			*str == '\f' || *str == '\r' || *str == ' ')
-		str++;
-	if (*str == '+')
-		str++;
-	else if (*str == '-')
+int	ft_atoi(const char *str)
+{
+	int			res;
+	int			signal;
+	int			i;
+
+	res = 0;
+	signal = 1;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		neg = 1;
-		str++;
+		if (str[i] == '-')
+			signal = -1;
+		i++;
 	}
-	while (*str != '\0' && *str >= '0' && *str <= '9')
-		n = n * 10 + (*str++ - '0');
-	if (neg == 1)
-		return (-n);
-	else
-		return (n);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = ((res * 10) + (str[i] - '0'));
+		i++;
+	}
+	return (res * signal);
 }
+
